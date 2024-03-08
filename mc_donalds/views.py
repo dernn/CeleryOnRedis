e566a19d68174ee488a2b1cf6a7f38f3e92af36d
dernn <fajj@mail.ru>
2024-03-08 16:15:11 +0600

    apply_async + countdown instead delay()

--- mc_donalds/views.py
+++ mc_donalds/views.py
@@ -6,6 +6,6 @@ from .tasks import hello, printer
 class IndexView(View):
     def get(self, request):
         # передаем аргумент для функции-задачи через delay()
-        printer.delay(10)
+        printer.apply_async([10], countdown=10)
         hello.delay()
         return HttpResponse('Hello!')
