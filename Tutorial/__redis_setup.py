8820e8fc8b11b0868c7c5caa553ce177d18b7a49
dernn <fajj@mail.ru>
2024-03-09 13:42:07 +0600

    celery beat_schedule [D10.5]

--- Tutorial/__redis_setup.py
+++ Tutorial/__redis_setup.py
@@ -11,6 +11,16 @@
 # А в другом — запустить Celery:
 # celery -A <project_name> worker -l INFO --pool=solo
 # (!) <project_name> здесь каталог конфигурации проекта (где settings.py etc.)
+# флаг -A означает application
+# флаг -l и его значение INFO указывает, что именно выводить в лог консоли
+# параметр --concurrency=10 указывает количество процессов, которые могут запускаться на worker'е
+# флаг -B для запуска периодических задач
+# (!) [WINDOWS:] в разных окнах терминала:
+#
+# celery -A <project_name> worker -l INFO --pool=solo
+# celery -A PROJECT beat -l INFO [--pool=solo(?)]
+#
+# Настройки Celery позволяют создавать несколько воркеров, несколько очередей с различными маршрутизациями.
 #
 # Start celery worker with --purge or --discard command how to remove task from queue
 #
--- Tutorial/celery.py
+++ Tutorial/celery.py
@@ -7,3 +7,12 @@ app = Celery('Tutorial')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'action_every_30_seconds': {  # имя периодической задачи
+        # параметры периодической задачи
+        'task': 'tasks.action',  # задачу вызываем из файла 'tasks.py', как и прежде
+        'schedule': 30,  # периодичность
+        'args': ("some_arg"),
+    },
+}
