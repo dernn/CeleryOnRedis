82283c13975617aaf48e65aacc75c0b5e9402085
dernn <fajj@mail.ru>
2024-03-09 14:35:36 +0600

    celery crontab template

--- Tutorial/celery.py
+++ Tutorial/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tutorial.settings')
 
@@ -15,5 +16,13 @@ app.conf.beat_schedule = {
         'schedule': 5,  # каждые 5 секунд
         'args': (5,),  # передаем аргумент в таску (длина счёта)
     },
-    # здесь может быть ещё одна таска
+    # здесь может быть ещё одна таска (или несколько)
+
+    # crontab: <ШАБЛОН>
+    'action_every_monday_8am': {
+        'task': 'action',
+        # каждый понедельник в 8.00 утра
+        'schedule': crontab(minute=0, hour=8, day_of_week='monday'),
+        'args': ('agrs'),
+    },
 }
